5bc1979727a61e76fc42dc5820bc10fd7344d25c
Mara Neagu <marateodoraneagu@gmail.com>
2023-05-02 15:37:19 +0300

    showMoreWorksFine

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -1,18 +1,22 @@
-import { Box, Typography, useTheme } from "@mui/material";
+import { Box, Button, Typography, useTheme } from "@mui/material";
 import Friend from "components/Friend";
 import FriendOnPost from "components/FriendOnPost";
 import WidgetWrapper from "components/WidgetWrapper";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setFriends } from "state";
+import { useNavigate } from "react-router-dom";
 
 const FriendListWidget = ({ userId }) => {
   const dispatch = useDispatch();
   const { palette } = useTheme();
   const token = useSelector((state) => state.token);
+  const navigate = useNavigate();
   const friends = useSelector((state) => state.user.friends);
   const loggedInUserId = useSelector((state) => state.user._id);
   const isProfileUser = userId === loggedInUserId;
+  const [showMore, setShowMore] = useState(false);
+
   const getFriends = async () => {
     const response = await fetch(
       `http://localhost:3001/users/${userId}/friends`,
@@ -40,7 +44,8 @@ const FriendListWidget = ({ userId }) => {
         Following
       </Typography>
       <Box display="flex" flexDirection="column" gap="1.5rem" mb="0.5rem">
-      {friends.map((friend) => (
+
+      {friends.slice(0, showMore ? friends.length : 3).map((friend)  => (
           isProfileUser ? (
             <Friend
               key={friend._id}
@@ -59,8 +64,28 @@ const FriendListWidget = ({ userId }) => {
             />
           )
         ))}
-        
       </Box>
+
+      {friends.length > 3 && ( // only show button if there are more than 3 friends
+        <Box 
+          mt="1.5rem"
+          display="flex" justifyContent="center"
+        >
+          <Button
+            onClick={() => navigate(`/showMoreFriends/${userId}`)}
+            sx={{
+              m: "0.5rem 0",
+              p: "0.5rem",
+              backgroundColor: palette.background.alt,
+              color: palette.login.button,
+              "&:hover": { backgroundColor: palette.login.buttonHover,
+                           color: palette.login.buttonTextHover },
+            }}
+          >
+            Show More
+          </Button>
+        </Box>
+      )}
     </WidgetWrapper>
   );
 };
