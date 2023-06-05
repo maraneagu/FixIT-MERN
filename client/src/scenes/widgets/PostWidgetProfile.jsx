aa17a39f910f16c3c4815c10e6db62c32ee5fb9b
cosminagheorghe <cosmi.2002@yahoo.com>
2023-06-05 13:52:33 +0300

    delete pop-up profile

--- client/src/scenes/widgets/PostWidgetProfile.jsx
+++ client/src/scenes/widgets/PostWidgetProfile.jsx
@@ -6,7 +6,7 @@ import {
   DeleteOutlined,
 } from "@mui/icons-material";
 import EditIcon from "@mui/icons-material/Edit";
-import ClassIcon from '@mui/icons-material/Class';
+import ClassIcon from "@mui/icons-material/Class";
 import {
   Box,
   Divider,
@@ -14,6 +14,11 @@ import {
   Typography,
   useTheme,
   Button,
+  useMediaQuery,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
 } from "@mui/material";
 import FlexBetween from "components/FlexBetween";
 import FriendOnPost from "components/FriendOnPost";
@@ -38,6 +43,7 @@ const PostWidgetProfile = ({
   comments,
 }) => {
   const [isComments, setIsComments] = useState(false);
+  const [deleteConfirmationOpen, setDeleteConfirmationOpen] = useState(false);
   const dispatch = useDispatch();
   const token = useSelector((state) => state.token);
 
@@ -51,35 +57,45 @@ const PostWidgetProfile = ({
   const medium = palette.neutral.medium;
   const main = palette.neutral.main;
   const primary = palette.primary.main;
-
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
   const patchLike = async () => {
-    const response = await fetch(`http://localhost:3001/posts/${postId}/like`, {
-      method: "PATCH",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ userId: loggedInUserId }),
-    });
+    const response = await fetch(
+      `http://localhost:3001/posts/${postId}/like`,
+      {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userId: loggedInUserId }),
+      }
+    );
     const updatedPost = await response.json();
     dispatch(setPost({ post: updatedPost }));
   };
+
+  const handleDeleteConfirmationOpen = () => {
+    setDeleteConfirmationOpen(true);
+  };
+
+  const handleDeleteConfirmationClose = () => {
+    setDeleteConfirmationOpen(false);
+  };
+
   const deletePost = async () => {
-    console.log("postid :", postId)
     const response = await fetch(`http://localhost:3001/posts/${postId}`, {
       method: "DELETE",
       headers: {
         Authorization: `Bearer ${token}`,
         "Content-Type": "application/json",
-        
       },
       body: JSON.stringify({ userId: loggedInUserId }),
     });
+
     if (response.ok) {
       const restPosts = await response.json();
-      window.location.reload();
-      dispatch(setPosts({ posts: restPosts}));
-      
+      dispatch(setPosts({ posts: restPosts }));
+      window.location.reload(); 
     }
   };
   return (
@@ -91,23 +107,23 @@ const PostWidgetProfile = ({
         userPicturePath={userPicturePath}
       />
 
-      <Typography 
-        color={main} 
-        variant="h5" 
-        fontWeight="500" 
+      <Typography
+        color={main}
+        variant="h5"
+        fontWeight="500"
         sx={{ mt: "1rem", width: "100%", wordWrap: "break-word" }}
       >
         {title}
       </Typography>
-      
+
       <Typography
         color={medium}
         display="flex"
         alignItems="center"
         sx={{ mt: "1.3rem", mb: "5px" }}
       >
-        <ClassIcon sx={{ color: main, mr: "8px" }}/>
-        {category ? category.charAt(0).toUpperCase() + category.slice(1) : ''}
+        <ClassIcon sx={{ color: main, mr: "8px" }} />
+        {category ? category.charAt(0).toUpperCase() + category.slice(1) : ""}
       </Typography>
 
       {picturePath && (
@@ -116,80 +132,97 @@ const PostWidgetProfile = ({
           height="auto"
           alt="post"
           style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
-          src={`http://localhost:3001/assets/${picturePath}`}
+          src={`http://localhost:3001/uploads/${picturePath}`}
         />
       )}
 
-      <FlexBetween mt="0.25rem">
-        <FlexBetween gap="1rem">
-          <FlexBetween gap="0.3rem">
-            <IconButton onClick={patchLike}>
-              {isLiked ? (
-                <FavoriteOutlined sx={{ color: primary }} />
-              ) : (
-                <FavoriteBorderOutlined />
-              )}
-            </IconButton>
-            <Typography>{likeCount}</Typography>
-          </FlexBetween>
+      <Box mt="1rem" sx={{ lineHeight: "1.5", wordWrap: "break-word" }}>
+        {description}
+      </Box>
 
-          <FlexBetween gap="0.3rem">
-            <IconButton onClick={() => setIsComments(!isComments)}>
-              <ChatBubbleOutlineOutlined />
-            </IconButton>
-            <Typography>{comments.length}</Typography>
-          </FlexBetween>
-        </FlexBetween>
+      <Divider sx={{ my: "1.5rem" }} />
 
-        <Box>
-        {isProfileUser && (
-          <IconButton
-            onClick={() => navigate(`/editpost/${postId}`)}
-            sx={{
-              "&:hover": {
-                cursor: "pointer",
-              },
-            }}
+      <FlexBetween>
+        <IconButton
+          size="small"
+          onClick={patchLike}
+          sx={{ color: isLiked ? primary : medium }}
+        >
+          {isLiked ? (
+            <FavoriteOutlined fontSize="small" />
+          ) : (
+            <FavoriteBorderOutlined fontSize="small" />
+          )}
+        </IconButton>
+
+        {isNonMobileScreens ? (
+          <>
+            <Button
+              variant="outlined"
+              size="small"
+              startIcon={<ChatBubbleOutlineOutlined />}
+              onClick={() => setIsComments(!isComments)}
+            >
+              Comments ({comments.length})
+            </Button>
+
+            {isProfileUser && (
+              <>
+                <IconButton
+                  size="small"
+                  onClick={() =>
+                    navigate(`/edit-post/${postId}`, { state: { post: { postId } } })
+                  }
+                  sx={{ color: main }}
+                >
+                  <EditIcon fontSize="small" />
+                </IconButton>
+
+                <IconButton
+                  size="small"
+                  onClick={handleDeleteConfirmationOpen}
+                  sx={{ color: main }}
+                >
+                  <DeleteOutlined fontSize="small" />
+                </IconButton>
+              </>
+            )}
+          </>
+        ) : (
+          <Button
+            variant="outlined"
+            size="small"
+            startIcon={<ChatBubbleOutlineOutlined />}
+            onClick={() => navigate(`/post/${postId}`)}
           >
-            <EditIcon/>
-          </IconButton>
+            Comments ({comments.length})
+          </Button>
         )}
-        {isProfileUser && (
 
-          <IconButton onClick={deletePost}>
-            <DeleteOutlined />
-          </IconButton>
-        )}</Box>
+        <IconButton size="small">
+          <ShareOutlined fontSize="small" />
+        </IconButton>
       </FlexBetween>
 
-      {isComments && (
-        <Box mt="0.5rem">
-          {comments.map((comment, i) => (
-            <Box key={`${name}-${i}`}>
-              <Divider />
-              <Typography sx={{ color: main, m: "0.5rem 0", pl: "1rem" }}>
-                {comment}
-              </Typography>
-            </Box>
-          ))}
-          <Divider />
-        </Box>
-      )}
-
-      {/* Show the button only on the home page */}
-      <Box
-        marginTop="10px"
-        marginBottom="10px"
-        display="flex"
-        justifyContent="center"
+      <Dialog
+        open={deleteConfirmationOpen}
+        onClose={handleDeleteConfirmationClose}
       >
-        <Button
-          variant="contained"
-          onClick={() => navigate(`/show/${postId}`)}
-        >
-          See the offer
-        </Button>
-      </Box>
+        <DialogTitle>Delete Confirmation</DialogTitle>
+        <DialogContent>
+          <Typography>
+            Are you sure you want to delete this post?
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDeleteConfirmationClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={deletePost} color="primary">
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
     </WidgetWrapper>
   );
 };
